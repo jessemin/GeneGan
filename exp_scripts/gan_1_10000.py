396795c39bca3c01fd881251df30a9730111f034
Jesik <jesikmin@stanford.edu>
2018-04-16 00:10:33 -0700

    added scripts

---
+++ exp_scripts/gan_1_10000.py
@@ -0,0 +1,9 @@
+import os
+
+os.chdir('../exp_notebooks')
+os.system('python gan_barebone.py\
+          -w=2001\
+          -save_m=gan_1_10000\
+          -save_srv=gan_1_10000\
+          -sample_num=10000\
+          -cuda=0'
